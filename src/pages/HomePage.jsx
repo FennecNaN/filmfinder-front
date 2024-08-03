d39aadb638ead4bc6cd5e7b912133c7216adc8d2
AlexF <fennecfr@gmail.com>
2024-08-04 00:33:30 +0200

    Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
         const response = await axios.get(`${config.apiUrl}/movies?page=${currentPage}&limit=6`);
         setMovies(response.data);
 
-        const totalResponse = await axios.get(`${config.apiUrl}/api/movies/count`);
+        const totalResponse = await axios.get(`${config.apiUrl}/movies/count`);
         setTotalPages(Math.ceil(totalResponse.data.count / 6));
       } catch (error) {
         console.error('Error fetching movies:', error);
