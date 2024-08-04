f99515195bb0da2a3c70c72567f358a0c5ca5e9a
AlexF <fennecfr@gmail.com>
2024-08-04 03:27:25 +0200

    updated config

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: process.env.VITE_API_URL
-  };
-  console.log('API URL:', config.apiUrl);
-  export default config;
-  
+  apiUrl: import.meta.env.VITE_API_URL
+};
+
+console.log('API URL:', config.apiUrl);
+export default config;
